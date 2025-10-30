79d88c2be83895b442ea242a70dd1843e3342ec4
Daniel Borsheim <daniel01.kodehode@gmail.com>
2025-10-30 09:55:14 +0100

    deploy

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require("express-rate-limit");
 
 const apiLimiter = rateLimit({
-  windowMS: 15 * 60 * 1000, // 15 minutes
-  max: 100,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100,
   message: "Rate limit exceeded. Please try again later",
 });
 
